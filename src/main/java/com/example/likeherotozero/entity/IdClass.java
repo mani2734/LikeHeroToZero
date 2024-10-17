4ac5796c49d49199c89e471a450da7aa08e62032
athmasb <manuel.scheibenreif@essity.com>
2024-10-17 22:03:10 +0200

    add Embeddable

--- src/main/java/com/example/likeherotozero/entity/IdClass.java
+++ src/main/java/com/example/likeherotozero/entity/IdClass.java
@@ -1,12 +1,19 @@
 package com.example.likeherotozero.entity;
 
+import jakarta.persistence.Embeddable;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Embeddable
 public class IdClass implements Serializable {
 
   private String country;
   private String year_;
+
+  public IdClass() {
+
+  }
 
   public IdClass(String country, String year_) {
     this.country = country;
