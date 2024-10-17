c2ce87ab3e28853e60c5935272105eca2ef37851
athmasb <manuel.scheibenreif@essity.com>
2024-10-17 21:29:42 +0200

    add functions to Co2AustossController (empty)

--- src/main/java/com/example/likeherotozero/controller/Co2AustossController.java
+++ src/main/java/com/example/likeherotozero/controller/Co2AustossController.java
@@ -1,6 +1,7 @@
 package com.example.likeherotozero.controller;
 
 import com.example.likeherotozero.dao.Co2AustossDAO;
+import com.example.likeherotozero.entity.Co2Austoss;
 import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
@@ -15,5 +16,21 @@ public class Co2AustossController implements Serializable {
   public Co2AustossDAO co2AustossDAO;
 
 
+
+  public Co2Austoss getNewestEntryForCountry(String country) {
+
+    return null;
+  }
+
+  public boolean addEntry(Co2Austoss co2Austoss) {
+    try {
+      //save
+    } catch(Exception e) {
+      System.out.println("Couldn't add entry");
+      e.printStackTrace();
+      return false;
+    }
+    return true;
+  }
 
 }
