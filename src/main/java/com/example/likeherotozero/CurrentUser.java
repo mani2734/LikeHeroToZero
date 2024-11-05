e61352eb3048a8c0845b077bbde11b84d9eea55f
athmasb <manuel.scheibenreif@essity.com>
2024-11-05 08:04:59 +0100

    add field to CurrentUser

--- src/main/java/com/example/likeherotozero/CurrentUser.java
+++ src/main/java/com/example/likeherotozero/CurrentUser.java
@@ -8,13 +8,28 @@ import java.io.Serializable;
 @Named
 @SessionScoped
 public class CurrentUser implements Serializable {
-  boolean admin;
+  private boolean admin;
+
+  private boolean guest;
 
   void reset() {
     this.admin = false;
+    this.guest = false;
   }
 
   boolean isAdmin() {
     return this.admin;
+  }
+
+  boolean isGuest() {
+    return this.guest;
+  }
+
+  public void setAdmin(boolean admin) {
+    this.admin = admin;
+  }
+
+  public void setGuest(boolean guest) {
+    this.guest = guest;
   }
 }
